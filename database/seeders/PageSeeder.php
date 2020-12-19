6fd3bef602c497f25aa2caa00694b8c8a0c56f8b
jmebia <jmebia@gmail.com>
2020-12-20 05:01:55 +0800

    added page seeder

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pages')->insert([
+            'page_code' => md5(Str::random(10)),
+            'name' => 'page',
+            'css' => 'body {
+                padding-top: 5rem;
+              }',
+            'content' => '<main role="main" class="container-fluid"> <main class="container"> <div class="starter-template text-center py-5 px-3"> <h1>Bootstrap starter template</h1> <p class="lead">Use this document as a way to quickly start any new project.<br>All you get is this text and a mostly barebones HTML document.</p></div></main> </main>',
+        ]);
+    }
+}
