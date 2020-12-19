ec13a15b5c1ee7ab4538940fac7c59284895d368
jmebia <jmebia@gmail.com>
2020-12-20 03:44:11 +0800

    added a working dynamic single pager

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Dynamic Site by JM Ebia</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  </head>
+
+  <body>
+
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Dynamic</a>
+        </div>
+    </nav>
+
+    <main role="main" class="container-fluid">
+    @isset($ds_header)
+        {!! $ds_header !!}
+    @endif
+
+    @isset($ds_body)
+        {!! $ds_body !!}
+    @else
+        <h1>Error 404 - No page content found.</h1>
+    @endif
+    </main><!-- /.container -->
+
+    @isset($ds_footer)
+        {!! $ds_footer !!}
+    @endif
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{page}/index', function($page) {
+
+    $ds_header = "<div class='bg-light p-5 my-2'> 
+    <p>You are now viewing</p>
+    <h1>" . $page . "</h1> </div>";
+    $ds_body = "<div class='container'> This is ". $page . "'s content. </div>";
+
+    // Footer component
+    // $ds_footer = "<div class='footer mt-auto py-3 bg-light'> You've reached the end of ". $page . ". </div>";
+
+    return view('layouts.main', compact('ds_header', 'ds_body'));
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
