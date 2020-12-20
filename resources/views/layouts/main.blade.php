4ded38a719286162fb104ea2e5bcf2bb27dae293
jmebia <jmebia@gmail.com>
2020-12-21 07:28:12 +0800

    updated header and main body in main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,40 +22,21 @@
   </head>
 
   <body>
-
-    <!-- As a link -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            @foreach ($pages_list as $link)
-            <li class="nav-item">
-              @if (strpos($_SERVER['REQUEST_URI'], $link->name) !== false)
-              <a class="nav-link active" aria-current="page" href="/{{$link->name}}/index">{{$link->name}}</a>
-              @else
-              <a class="nav-link" href="/{{$link->name}}/index">{{$link->name}}</a>
-              @endif
-            </li>
-            @endforeach
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <header>
+      @isset($ds_header)
+        {!! $ds_header !!}
+      @else
+        @include('shared.default_nav')
+      @endif
+      
+    <header>
 
     <main role="main" class="container-fluid">
-    @isset($ds_header)
-        {!! $ds_header !!}
-    @endif
-
-    @isset($ds_body)
-        {!! $ds_body !!}
-    @else
-        <h4>Error 404 - No page content found.</h4>
-    @endif
+      @isset($ds_body)
+          {!! $ds_body !!}
+      @else
+          <h4>Error 404 - No page content found.</h4>
+      @endif
     </main><!-- /.container -->
 
     @isset($ds_footer)
