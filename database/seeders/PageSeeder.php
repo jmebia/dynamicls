090548d011db0b0d42b380c1b1c62f7e6c40d6ca
jmebia <jmebia@gmail.com>
2020-12-29 22:30:25 +0800

    updated header

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -18,11 +18,13 @@ class PageSeeder extends Seeder
         // seed example bootstrap page
         DB::table('pages')->insert([
             'page_code' => md5(Str::random(10)),
+            'is_home' => true,
             'name' => 'page',
             'css' => 'body {
-                padding-top: 5rem;
+                padding-top: 0;
               }',
             'content' => '<main role="main" class="container-fluid"> <main class="container"> <div class="starter-template text-center py-5 px-3"> <h1>Bootstrap starter template</h1> <p class="lead">Use this document as a way to quickly start any new project.<br>All you get is this text and a mostly barebones HTML document.</p></div></main> </main>',
-        ]);
+            'created_at' => date("Y-m-d h:m:s"),
+            ]);
     }
 }
